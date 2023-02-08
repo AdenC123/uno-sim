1d0678e255bcac6f469dd4ff04b7f0638bd21faa
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-02-08 12:40:53 -0800

    remove code from Main for task 1

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,11 +1,8 @@
 package ui;
 
-import model.Player;
 
 public class Main {
     public static void main(String[] args) {
-        Player testPlayer = new Player(1);
-        testPlayer.drawCards(30);
-        System.out.println(testPlayer.handToString());
+
     }
 }
