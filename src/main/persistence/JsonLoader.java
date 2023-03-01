995fd6bd80bad67c8ec9027303ba1edd80350838
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-03-01 13:50:47 -0800

    JSON loader and example data

---
+++ src/main/persistence/JsonLoader.java
@@ -0,0 +1,21 @@
+package persistence;
+
+import model.Game;
+
+import java.io.IOException;
+
+// Loads games stored as JSON data from a file
+// Modeled on JsonSerializationDemo project
+public class JsonLoader {
+
+    // Effects: constructs loader to load from given filename
+    public JsonLoader(String filename) {
+        //stub
+    }
+
+    // Effects: reads and returns the game stored in the file,
+    //          throws IOException if a file reading error occurs
+    public Game load() throws IOException {
+        return null; //stub
+    }
+}
